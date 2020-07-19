2997be3d9daf7619443f582833d39ef082f16ec5
Gaetz <gblaisec@artfx.fr>
2020-07-19 15:55:08 +0200

    24: AudioSystem, SoundEvent (use linker/input vers to x64 dll), AudioComponent, Game update

---
+++ SimpleEngineWithOpenGL/SimpleEngineWithOpenGL-024/Game.cpp
@@ -0,0 +1,240 @@
+#include "Game.h"
+#include "Actor.h"
+#include "Timer.h"
+#include "Assets.h"
+#include "MeshComponent.h"
+#include "Cube.h"
+#include "Sphere.h"
+#include "Plane.h"
+
+bool Game::initialize()
+{
+	bool isWindowInit = window.initialize();
+	bool isRendererInit = renderer.initialize(window);
+	bool isAudioInit = audioSystem.initialize();
+	return isWindowInit && isRendererInit && isAudioInit; // Return bool && bool && bool ...to detect error
+}
+
+void Game::load()
+{
+	Assets::loadShader("Res\\Shaders\\Sprite.vert", "Res\\Shaders\\Sprite.frag", "", "", "", "Sprite");
+	Assets::loadShader("Res\\Shaders\\BasicMesh.vert", "Res\\Shaders\\BasicMesh.frag", "", "", "", "BasicMesh");
+	Assets::loadShader("Res\\Shaders\\Phong.vert", "Res\\Shaders\\Phong.frag", "", "", "", "Phong");
+
+	Assets::loadTexture(renderer, "Res\\Textures\\Default.png", "Default");
+	Assets::loadTexture(renderer, "Res\\Textures\\Cube.png", "Cube");
+	Assets::loadTexture(renderer, "Res\\Textures\\HealthBar.png", "HealthBar");
+	Assets::loadTexture(renderer, "Res\\Textures\\Plane.png", "Plane");
+	Assets::loadTexture(renderer, "Res\\Textures\\Radar.png", "Radar");
+	Assets::loadTexture(renderer, "Res\\Textures\\Sphere.png", "Sphere");
+	
+	Assets::loadMesh("Res\\Meshes\\Cube.gpmesh", "Mesh_Cube");
+	Assets::loadMesh("Res\\Meshes\\Plane.gpmesh", "Mesh_Plane");
+	Assets::loadMesh("Res\\Meshes\\Sphere.gpmesh", "Mesh_Sphere");
+	
+	camera = new Camera();
+
+	Cube* a = new Cube();
+	a->setPosition(Vector3(200.0f, 105.0f, 0.0f));
+	a->setScale(100.0f);
+	Quaternion q(Vector3::unitY, -Maths::piOver2);
+	q = Quaternion::concatenate(q, Quaternion(Vector3::unitZ, Maths::pi + Maths::pi / 4.0f));
+	a->setRotation(q);
+	
+	Sphere* b = new Sphere();
+	b->setPosition(Vector3(200.0f, -75.0f, 0.0f));
+	b->setScale(3.0f);
+
+	// Floor and walls
+
+	// Setup floor
+	const float start = -1250.0f;
+	const float size = 250.0f;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			Plane* p = new Plane();
+			p->setPosition(Vector3(start + i * size, start + j * size, -100.0f));
+		}
+	}
+
+	// Left/right walls
+	q = Quaternion(Vector3::unitX, Maths::piOver2);
+	for (int i = 0; i < 10; i++)
+	{
+		Plane* p = new Plane();
+		p->setPosition(Vector3(start + i * size, start - size, 0.0f));
+		p->setRotation(q);
+
+		p = new Plane();
+		p->setPosition(Vector3(start + i * size, -start + size, 0.0f));
+		p->setRotation(q);
+	}
+
+	q = Quaternion::concatenate(q, Quaternion(Vector3::unitZ, Maths::piOver2));
+	// Forward/back walls
+	for (int i = 0; i < 10; i++)
+	{
+		Plane* p = new Plane();
+		p->setPosition(Vector3(start - size, start + i * size, 0.0f));
+		p->setRotation(q);
+
+		p = new Plane();
+		p->setPosition(Vector3(-start + size, start + i * size, 0.0f));
+		p->setRotation(q);
+	}
+
+	// Setup lights
+	renderer.setAmbientLight(Vector3(0.2f, 0.2f, 0.2f));
+	DirectionalLight& dir = renderer.getDirectionalLight();
+	dir.direction = Vector3(0.0f, -0.707f, -0.707f);
+	dir.diffuseColor = Vector3(0.78f, 0.88f, 1.0f);
+	dir.specColor = Vector3(0.8f, 0.8f, 0.8f);
+
+	// UI elements
+	Actor* ui = new Actor();
+	ui->setPosition(Vector3(-350.0f, -350.0f, 0.0f));
+	SpriteComponent* sc = new SpriteComponent(*ui, Assets::getTexture("HealthBar"));
+
+	ui = new Actor();
+	ui->setPosition(Vector3(375.0f, -275.0f, 0.0f));
+	ui->setScale(0.75f);
+	sc = new SpriteComponent(*ui, Assets::getTexture("Radar"));
+}
+
+void Game::processInput()
+{
+	// SDL Event
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		}
+	}
+	// Keyboard state
+	const Uint8* keyboardState = SDL_GetKeyboardState(nullptr);
+	// Escape: quit game
+	if (keyboardState[SDL_SCANCODE_ESCAPE])
+	{
+		isRunning = false;
+	}
+	// Actor input
+	isUpdatingActors = true;
+	for (auto actor : actors)
+	{
+		actor->processInput(keyboardState);
+	}
+	isUpdatingActors = false;
+}
+
+void Game::update(float dt)
+{
+	// Update audio
+	audioSystem.update(dt);
+
+	// Update actors 
+	isUpdatingActors = true;
+	for(auto actor: actors) 
+	{
+		actor->update(dt);
+	}
+	isUpdatingActors = false;
+
+	// Move pending actors to actors
+	for (auto pendingActor: pendingActors)
+	{
+		pendingActor->computeWorldTransform();
+		actors.emplace_back(pendingActor);
+	}
+	pendingActors.clear();
+
+	// Delete dead actors
+	vector<Actor*> deadActors;
+	for (auto actor : actors)
+	{
+		if (actor->getState() == Actor::ActorState::Dead)
+		{
+			deadActors.emplace_back(actor);
+		}
+	}
+	for (auto deadActor : deadActors)
+	{
+		delete deadActor;
+	}
+}
+
+void Game::render()
+{
+	renderer.beginDraw();
+	renderer.draw();
+	renderer.endDraw();
+}
+
+void Game::loop()
+{
+	Timer timer;
+	float dt = 0;
+	while (isRunning)
+	{
+		float dt = timer.computeDeltaTime() / 1000.0f;
+		processInput();
+		update(dt);
+		render();
+		timer.delayTime();
+	}
+}
+
+void Game::unload()
+{
+	// Delete actors
+	// Because ~Actor calls RemoveActor, have to use a different style loop
+	while (!actors.empty())
+	{
+		delete actors.back();
+	}
+
+	// Resources
+	Assets::clear();
+}
+
+void Game::close()
+{
+	renderer.close();
+	window.close();
+	SDL_Quit();
+}
+
+void Game::addActor(Actor* actor)
+{
+	if (isUpdatingActors)
+	{
+		pendingActors.emplace_back(actor);
+	}
+	else
+	{
+		actors.emplace_back(actor);
+	}
+}
+
+void Game::removeActor(Actor* actor)
+{
+	// Erase actor from the two vectors
+	auto iter = std::find(begin(pendingActors), end(pendingActors), actor);
+	if (iter != end(pendingActors))
+	{
+		// Swap to end of vector and pop off (avoid erase copies)
+		std::iter_swap(iter, end(pendingActors) - 1);
+		pendingActors.pop_back();
+	}
+	iter = std::find(begin(actors), end(actors), actor);
+	if (iter != end(actors))
+	{
+		std::iter_swap(iter, end(actors) - 1);
+		actors.pop_back();
+	}
+}
