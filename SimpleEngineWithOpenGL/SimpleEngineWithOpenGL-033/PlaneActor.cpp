6eca65dcbb4c8d2a9bd1496b759ac3bffaa03412
Gaetz <gblaisec@artfx.fr>
2020-07-28 10:08:08 +0200

    033: Collision algorithms

---
+++ SimpleEngineWithOpenGL/SimpleEngineWithOpenGL-033/PlaneActor.cpp
@@ -0,0 +1,10 @@
+#include "PlaneActor.h"
+#include "MeshComponent.h"
+#include "Assets.h"
+
+PlaneActor::PlaneActor() : Actor()
+{
+	setScale(10.0f);
+	MeshComponent* mc = new MeshComponent(*this);
+	mc->setMesh(&Assets::getMesh("Mesh_Plane"));
+}
