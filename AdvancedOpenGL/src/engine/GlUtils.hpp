917a64ae21bbfd25970e2bf9a3a1111ecab654b5
Gaetz <gaetz@rufflerim.com>
2020-08-19 17:52:41 +0200

    Add opengl extension utils

---
+++ AdvancedOpenGL/src/engine/GlUtils.hpp
@@ -0,0 +1,25 @@
+// Check if extension is supported
+int isExtensionSupported(const char* extname)
+{
+    GLint numExtensions;
+    GLint i;
+
+    glGetIntegerv(GL_NUM_EXTENSIONS, &numExtensions);
+
+    for (i = 0; i < numExtensions; i++)
+    {
+        const GLubyte * e = glGetStringi(GL_EXTENSIONS, i);
+        if (!strcmp((const char *)e, extname))
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+// Query the adress of extensions fonctions
+GL3WglProc getProcAddress(const char * funcname)
+{
+    return gl3wGetProcAddress(funcname);
+}
