11a15f6b21ca0884ee6aea99c1c8dd66390bcc2e
Gaetz <gaetz@rufflerim.com>
2020-08-28 06:50:14 +0200

    OpenGL 4.4 works on laptop, not 4.5

--- AdvancedOpenGL/src/engine/WindowSdl.cpp
+++ AdvancedOpenGL/src/engine/WindowSdl.cpp
@@ -28,7 +28,7 @@ bool WindowSdl::init(int xPos, int yPos, int width, int height, bool isFullscree
 
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 4);
 
         SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
         SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
