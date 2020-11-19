8621ccd86a473c52593bba94329ba64a97157f6d
Gaetz <gaetz@rufflerim.com>
2020-11-19 07:47:55 +0100

    Create SSL project

---
+++ IntroShadersOpenGL/03_SDL2/src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "engine/game.h"
+#include "engine/window.h"
+
+using std::cout;
+using std::endl;
+
+int main(int argc = 0, char **argv = nullptr) {
+    if (argc > 0) {
+        for (int i = 0; i < argc; ++i) {
+            cout << argv[i] << endl;
+        }
+    }
+
+    Window window;
+    Game game;
+
+    cout << "Hello handmade world" << endl;
+    window.display();
+    game.display();
+
+    return 0;
+}
