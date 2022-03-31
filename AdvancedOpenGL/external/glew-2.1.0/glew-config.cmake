91d56664257165b142c9b29088ba2cfed793b919
Gaëtan Blaise-Cazalet <gaetan.blaisecazalet@gmail.com>
2022-03-31 10:09:56 +0200

    Code example + cmake fix

--- AdvancedOpenGL/external/glew-2.1.0/glew-config.cmake
+++ AdvancedOpenGL/external/glew-2.1.0/glew-config.cmake
@@ -4,7 +4,7 @@ set(GLEW_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/include")
 if (${CMAKE_SIZEOF_VOID_P} MATCHES 8)
   set(GLEW_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/Release/x64/glew32.lib")
 else ()
-  set(GLEW_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/Release/Win32/glew32.lib")
+  set(GLEW_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/Release/x64/glew32.lib")
 endif ()
 
 string(STRIP "${GLEW_LIBRARIES}" GLEW_LIBRARIES)
