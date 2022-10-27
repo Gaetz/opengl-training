7e388d27a32cb6ffce9304fc760329b83ecbcfb3
Gaëtan Blaise-Cazalet <gaetan.blaisecazalet@gmail.com>
2022-10-27 10:18:16 +0200

    Log for cube mesh

--- AdvancedOpenGL/src/game/CubeMesh.cpp
+++ AdvancedOpenGL/src/game/CubeMesh.cpp
@@ -1,4 +1,5 @@
 #include "CubeMesh.h"
+#include "../engine/Log.h"
 
 void CubeMesh::load() {
   glGenVertexArrays(1, &vao);
@@ -58,6 +59,8 @@ void CubeMesh::load() {
   glGenBuffers(1, &buffer);
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+
+  LOG(Info) << "vertexPositions size " << sizeof(vertexPositions);
 
   // Setup vertex attribute
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
